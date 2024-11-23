c5c4881dd1321bb526ea2adb117a17a3cd030763
anitaloz <anitalozova@gmail.com>
2024-11-23 16:17:47 +0300

    commit

--- футер+слайдер/second-slider.js
+++ футер+слайдер/second-slider.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $(".slider").slick({
         infinite: true,
+        arrows : false,
         responsive: [{
             breakpoint: 900,
             settings: {
@@ -20,37 +21,20 @@ $(document).ready(function(){
 $(document).ready(function(){
     $(".slider2").slick({
         infinite: true,
+        arrows : false,
         responsive: [{
             breakpoint: 900,
             settings: {
                 infinite: true,
-                slidesToShow: 3,
+                slidesToShow: 2.7,
                 autoplay: true,
                 speed: 2000,
             }
         }],
         
-        slidesToScroll: 2,
-        slidesToShow: 5.5,
+        slidesToScroll: 1,
+        slidesToShow: 5,
         autoplay: true,
         speed: 2000,
     });
 });
-/*$(document).ready(function(){
-    $(".slider").slick({
-        infinite: true,
-        responsive: [{
-            breakpoint: 900,
-            settings: {
-                dots: true,
-                infinite: true,
-                slidesToShow: 1,
-                autoplay: true,
-                autoplaySpeed: 2000,
-            }
-        }],
-        slidesToShow: 3,
-        autoplay: true,
-        autoplaySpeed: 2000,
-    });
-});*/
