902f6d742e2eb63a89b5d1ce68fc48b7dcc019bb
nikayacenko <yacenkonika05@mail.ru>
2024-11-28 11:33:41 +0300

    Update accordeon-FAQ.js

--- accordeon-FAQ.js
+++ accordeon-FAQ.js
@@ -1,16 +1,19 @@
+
 $(document).ready(function() {
-
- // "Аккордион".
-    let cell = document.getElementsByClassName("accordion-cell")[0];
-    let text= document.getElementsByClassName("accordion-text")[0];
-    $(".accordion h3:first").addClass("active");
+    
+    $(".accordion .accordion-cell:first").addClass("active");
+    $(".accordion h3:first").addClass("active-acc-text");
     $(".accordion p:not(:first)").hide();
-    $(".accordion h3").click(function () {
-    $(this).next("p").slideToggle("slow")
-        .siblings("p:visible").slideUp("slow");
-    $(this).toggleClass("active");
-    $(this).siblings("h3").removeClass("active");
-    });
 
-   
-}); 
+    $(".accordion h3").click(function() {
+        $(this).parent(".accordion-cell").siblings(".accordion-cell").removeClass("active");
+        $(this).parent(".accordion-cell").toggleClass("active");
+
+        $(this).next("p").slideToggle("slow");
+        $(this).parent(".accordion-cell").siblings(".accordion-cell").children("p:visible").slideUp("slow");
+
+        $(this).toggleClass("active-acc-text");
+        $(this).parent(".accordion-cell").siblings(".accordion-cell").children("h3").removeClass("active-acc-text");
+
+    });
+});
